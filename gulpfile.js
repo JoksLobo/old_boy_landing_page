662f51f65e716973ac1e360c9dffd8de37a50378
Joks Lobo <joks@exemplo.com>
2024-07-15 15:20:21 -0300

    adicionado sessões hero, plot e characters

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles(){
     return gulp.src('./src/styles/*.scss') //recupera os arquivos scss
@@ -7,7 +8,13 @@ function styles(){
         .pipe(gulp.dest('./dist/css')); //envia os arquivos compilados à pasta de destino
 }
 
-exports.default = styles;
+function images(){
+    return gulp.src('./src/images/**/*') //recupera pega todos os arquivos de imagem
+        .pipe(imagemin()) //minifica as imagens
+        .pipe(gulp.dest('./dist/images')); //envia as imagens minificadas à pasta de destino
+}
+
+exports.default = gulp.parallel(styles, images);
 exports.watch = function () {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
